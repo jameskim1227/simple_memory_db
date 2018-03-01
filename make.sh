b2e67d68d55d6832c5fadd9058c087f6697e33d8
root <root@localhost.localdomain>
2018-03-01 21:15:03 +0900

    add apr library to

--- make.sh
+++ make.sh
@@ -1,4 +1,7 @@
 #! /bin/bash -x
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/lib
+
 gcc -o smd_server smd_server.c -Wall -I./ -I./include/apr-1/ -L./lib -lapr-1
 
 gcc -o smd_client smd_client.c -Wall
